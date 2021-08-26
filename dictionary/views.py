9923a971e42ef763cafef6c0e7211f0f9bd6f6fb
MdAbdulMalwk <gravityd000@gmail.com>
2021-08-26 16:00:25 +0600

    View Code

---
+++ dictionary/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from PyDictionary import PyDictionary
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def word(request):
+    search = request.GET.get('search')
+    dictionary = PyDictionary()
+    meaning = dictionary.meaning(search)
+    synonyms = dictionary.synonym(search)
+    antonyms = dictionary.antonym(search)
+    context = {
+        'input':search,
+        'meaning': meaning,
+        'synonyms': synonyms,
+        'antonyms': antonyms
+    }
+    return render(request, 'word.html', context)
+
+def back(request):
+    return render(request, 'index.html')
